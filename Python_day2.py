79790a72d45f9bbe03cfaa1fc88e4a2d7afd9517
KoOhsw <ko.ohsw@gmail.com>
2023-07-25 09:40:22 +0900

    화요일 수업

---
+++ Python_day2.py
@@ -0,0 +1,28 @@
+# Python_day2.py
+
+print('==== 함수 정의2 ====')
+x=1
+def func(a):
+    return a+x
+print(func(1))
+#지역변수가 있을경우 지역변수 우선 Local -> Global -> Built in 순 조회
+def func2(a):
+    x=5
+    return a+x
+print(func2(1))
+
+#기본값 지정-초기값 지점
+def times(a=10,b=20):
+    return a*b
+print(times())
+print(times(5))
+print(times(5,6))
+print(times(b=10))
+
+#키워드 인자
+def connectURI(server,port):
+    strURL = 'http://' + server + ':' + port
+    return strURL
+
+print(connectURI('credu.com','80'))
+print(connectURI(port = '8080', server = 'credu.com'))
