99a4a4e61880e0353e81949deea1d11b362a3ea0
KoOhsw <ko.ohsw@gmail.com>
2023-07-25 16:22:34 +0900

    실행파일 생성

--- Python_day2_2.py
+++ Python_day2_2.py
@@ -80,4 +80,46 @@ account1 = BankAccount(100, "전우치", 15000)
 account1.deposit(5000)
 account1.withdraw(3000)
 
+#_클래스이름__속성명으로 접근이 가능하다 => 완벽하게 숨길수 없다...
+#account1._BankAccount__balance = 1500000
+#print(account1._BankAccount__balance)
 print(account1)
+
+print('==== 상속 ====')
+class Person:
+    def __init__(self, name, phoneNumber):
+        self.name = name
+        self.phoneNumber = phoneNumber
+
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+    
+    def working(self):
+        print('현재 작업중')
+    
+    def coding(self):
+        print('현재 코딩중')
+
+
+class Student(Person):
+    #상속받고 덮어쓰기(재정의, override)
+    def __init__(self, name, phoneNumber, subject, studentID):
+        Person.__init__(self, name, phoneNumber)
+        self.subject = subject
+        self.studentID = studentID
+    
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print('Info(Subject:{0}, StudentID : {1}'.format(self.subject,self.studentID))
+
+#인스턴스 생성
+p = Person("전우치", "010-222-1234")
+s = Student("이순신", "010-111-1234", "빅데이터", "201234")
+
+# print(p.__dict__)
+# print(s.__dict__)
+
+p.printInfo()
+s.printInfo()
+s.coding()
+s.working()
